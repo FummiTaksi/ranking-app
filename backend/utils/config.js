84cca41f261df67460f8470a3b61ded85cbab6c7
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2020-04-16 21:37:33 +0300

    connect to devDB

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -6,7 +6,7 @@ let { PORT, MONGOLAB_URL } = process.env;
 const { MONGOLAB_TEST_URL } = process.env;
 if (process.env.NODE_ENV === 'test') {
   PORT = process.env.TEST_PORT;
-  MONGOLAB_URL = 'mongodb://database:27017/testDB';
+  MONGOLAB_URL = 'mongodb://database:27017/devDB';
 }
 
 MONGOLAB_URL = 'mongodb://database:27017/devDB';
