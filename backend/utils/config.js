c7efea7a8f84ec7f94ef3583566c3c3763c8603b
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2020-04-17 16:30:34 +0300

    refactor elseif statement when defining MONGOLAB_URL and PORT

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,13 +3,15 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 let { PORT, MONGOLAB_URL } = process.env;
-const { MONGOLAB_TEST_URL } = process.env;
 if (process.env.NODE_ENV === 'test') {
   PORT = process.env.TEST_PORT;
+  MONGOLAB_URL = 'mongodb://database:27017/testDB';
+} else if (process.env.NODE_ENV === 'e2etest') {
+  PORT = process.env.TEST_PORT;
+  MONGOLAB_URL = 'mongodb://database:27017/devDB';
+} else {
   MONGOLAB_URL = 'mongodb://database:27017/devDB';
 }
-
-MONGOLAB_URL = 'mongodb://database:27017/devDB';
 
 module.exports = {
   PORT,
