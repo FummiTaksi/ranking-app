f7e4d208b69d68beb884ee53323e6234bfc3b69c
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2020-04-21 17:56:48 +0300

    add debugging prints

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,6 +4,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 let { PORT, MONGOLAB_URL } = process.env;
 if (process.env.NODE_ENV === 'test') {
+  console.log('asetan testiportin', process.env.TEST_PORT);
+  console.log('envit', process.env);
   PORT = process.env.TEST_PORT;
   MONGOLAB_URL = 'mongodb://database:27017/testDB';
 } else if (process.env.NODE_ENV === 'e2etest') {
