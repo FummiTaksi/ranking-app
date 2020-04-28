713b7aa01f11ac16db2026aa7511cd7d3f3db731
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2020-04-28 21:47:22 +0300

    fix lint errors

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,8 +1,8 @@
 console.log('process.env', process.env);
 
 
-const BACKEND_URL = process.env.NODE_ENV === 'production' ? 'http://localhost:3001/api' : '/api'
+const BACKEND_URL = process.env.NODE_ENV === 'production' ? 'http://localhost:3001/api' : '/api';
 
 module.exports = {
- BACKEND_URL
-}
+  BACKEND_URL,
+};
