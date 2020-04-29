a0ca91e84bb317a2c9a8dfdb330e213264b69ea5
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2020-04-29 18:31:21 +0300

    remove console log

--- frontend/src/services/loginService.js
+++ frontend/src/services/loginService.js
@@ -4,7 +4,6 @@ import { BACKEND_URL } from '../utils/config';
 const baseUrl = `${BACKEND_URL}/login`;
 
 const login = async (credentials) => {
-  console.log('BACKENDURL IS', BACKEND_URL);
   const response = await axios.post(baseUrl, credentials);
   return response.data;
 };
