d783779a15ccc20fd3ec408df5acc87e88b930c1
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2020-05-05 13:58:54 +0300

    add removeOne model function

--- backend/models/ranking.js
+++ backend/models/ranking.js
@@ -16,6 +16,10 @@ rankingSchema.pre('remove', async function (next) {
   await this.model('Position').remove({ ranking: this._id }, next);
 });
 
+rankingSchema.pre('deleteOne', { document: true }, async function (next) {
+  await this.model('Position').remove({ ranking: this._id }, next);
+});
+
 const Ranking = mongoose.model('Ranking', rankingSchema);
 
 module.exports = Ranking;
