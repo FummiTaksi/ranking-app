0e8e4c6606dae76147968bd414dc889da4fdb4be
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2020-05-13 19:10:57 +0300

    implement more reducer tests

--- frontend/src/tests/rankingReducer.test.js
+++ frontend/src/tests/rankingReducer.test.js
@@ -1,7 +1,11 @@
 import rankingReducer from '../reducers/rankingReducer';
 
-describe('rankingReducer ', () => {
+describe('rankingReducer', () => {
   const initialState = { allRankings: [], selectedRanking: {}, loading: false };
+  const ranking = {
+    name: 'reducerTestRanking',
+    _id: '1',
+  };
   it(' should return a proper initial state when called with undefined state', () => {
     const action = {
       type: 'DO_NOTHING',
@@ -9,18 +13,53 @@ describe('rankingReducer ', () => {
     const newState = rankingReducer(undefined, action);
     expect(newState).toEqual(initialState);
   });
-  it(' CREATE_RANKING works properly', () => {
-    const ranking = {
-      name: 'reducerTestRanking',
-      _id: '1',
-    };
+  describe(' When creating ranking', () => {
+    it(' CREATING_RANKING sets loading to true', () => {
+      const action = {
+        type: 'CREATING_RANKING',
+      };
+      const newState = rankingReducer(initialState, action);
+      expect(newState.loading).toBeTruthy();
+    });
+
+    describe('CREATE_RANKING', () => {
+      let newState;
+      beforeAll(() => {
+        const loadingState = { allRankings: [], selectedRanking: {} , loading: true }
+        const action = {
+          type: 'CREATE_RANKING',
+          content: {
+            ranking
+          }
+        };
+        newState = rankingReducer(loadingState, action);
+      });
+      it(' adds rankingObject to the list', () => {
+        expect(newState.allRankings.length).toEqual(1);
+      });
+      it(' puts loading to false', () => {
+        expect(newState.loading).toBeFalsy()
+      })
+    })
+
+  })
+
+  it(' SETTING_RANKINGS sets loading to true', () => {
     const action = {
-      type: 'CREATE_RANKING',
+      type: 'SETTING_RANKINGS',
+    };
+    const newState = rankingReducer(initialState, action);
+    expect(newState.loading).toBeTruthy();
+  });
+  it(' SET_RANKINGS adds an array of rankings', () => {
+    const rankings = [ranking, ranking]
+    const action = {
+      type: 'SET_RANKINGS',
       content: {
-        ranking
+        rankings
       }
     };
     const newState = rankingReducer(initialState, action);
-    expect(newState.allRankings.length).toEqual(1);
+    expect(newState.allRankings.length).toBe(2);
   });
 });
