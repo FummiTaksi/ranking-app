d3e42866dafdb2a8dbe3fae777eff9705e39243e
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2020-05-14 19:04:10 +0300

    implement returnDateString function

--- backend/services/fileService.js
+++ backend/services/fileService.js
@@ -7,4 +7,12 @@ const convertBase64ToExcel = (base64String) => {
   return xlsx.utils.sheet_to_json(xlsFromBase64.Sheets['Rating järjestys']);
 };
 
-module.exports = { convertBase64ToExcel };
+const returnDateString = (base64String) => {
+  const withoutPrefix = base64String.substring(37);
+  const options = { type: 'base64' };
+  const xlsFromBase64 = xlsx.read(withoutPrefix, options);
+  const json = xlsx.utils.sheet_to_json(xlsFromBase64.Sheets.Pelaajat);
+  return json[0]['SPTL RATINGS'];
+};
+
+module.exports = { convertBase64ToExcel, returnDateString };
--- backend/tests/unit/services/fileService.test.js
+++ backend/tests/unit/services/fileService.test.js
@@ -1,10 +1,15 @@
 const testHelpers = require('../../helpers/testHelpers');
 const fileService = require('../../../services/fileService');
 
-describe('FileService ', () => {
-  test(' converts base64 to json without ', () => {
+describe('FileService', () => {
+  test(' converts base64 to json ', () => {
     const base64 = testHelpers.getRatingBase64();
     const fileJson = fileService.convertBase64ToExcel(base64);
     expect(fileJson.length).toEqual(11);
+  });
+  test(' returnDateString returns correct string variable', () => {
+    const base64 = testHelpers.getKoskiBase64();
+    const string = fileService.returnDateString(base64);
+    expect(string).toEqual('Viimeinen päivitys: Kosken_Malja_GP_su - 06.01.2019');
   });
 });
