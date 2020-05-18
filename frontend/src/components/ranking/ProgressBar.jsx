79e031cd26ba544667cd4620b5e8a8825c29a4c4
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2020-05-18 21:20:53 +0300

    add div id

--- frontend/src/components/ranking/ProgressBar.jsx
+++ frontend/src/components/ranking/ProgressBar.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ProgressBar({ completed }) {
+  if (completed) {
+    return (
+      <div id="completed">
+        <h1>Uploading complete</h1>
+      </div> 
+    )
+  }
   return (
-    <h1>{`Completed: ${completed}`}</h1>
+    <div id="notCompleted">
+      <h1>Uploading not complete</h1>
+    </div>
   );
 }
 
