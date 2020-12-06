54bce0470cd2bab757572c5c69ccf4e4517ef3fa
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2020-12-06 12:06:11 +0200

    refactor to function and to use hooks

--- frontend/src/containers/signin/SignInForm.jsx
+++ frontend/src/containers/signin/SignInForm.jsx
@@ -1,66 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Input from '../../components/form/Input';
 import { login } from '../../reducers/loginReducer';
 
-class SignInForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-    };
-    this.handleFormChange = this.handleFormChange.bind(this);
-    this.logIn = this.logIn.bind(this);
-  }
+function SignInForm(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { logIn } = props;
 
-  handleFormChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    logIn({ username, password });
+    setUsername('');
+    setPassword('');
+  };
 
-  logIn(e) {
-    e.preventDefault();
-    const { logIn } = this.props;
-    logIn(this.state);
-    this.setState({
-      username: '',
-      password: '',
-    });
-  }
-
-  render() {
-    const { username, password } = this.state;
-    return (
-      <div>
-        <h2>
-          Signing in is only available for admin!
-        </h2>
-        <form onSubmit={this.logIn}>
-          <Input
-            type="input"
-            text="Username:"
-            name="username"
-            value={username}
-            onChange={this.handleFormChange}
-          />
-          <Input
-            type="password"
-            text="Password:"
-            name="password"
-            value={password}
-            onChange={this.handleFormChange}
-          />
-          <button type="submit">
-            Log in
-            {' '}
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>
+        Signing in is only available for admin!
+      </h2>
+      <form onSubmit={onSubmit}>
+        <Input
+          type="input"
+          text="Username:"
+          name="username"
+          value={username}
+          onChange={event => setUsername(event.target.value)}
+        />
+        <Input
+          type="password"
+          text="Password:"
+          name="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+        <button type="submit">
+          Log in
+          {' '}
+        </button>
+      </form>
+    </div>
+  );
 }
 
 SignInForm.propTypes = {
